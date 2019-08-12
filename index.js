ac7ac52cf7c9107216238de2ff658219ed7e08be
Kenn <kenn+git@code-crew.org>
2019-08-12 14:27:12 -0500

    kenn updates

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// The prompt is not being saved into a variable
 prompt("Welcome to Jessica's program. Press 1 to count. Press 2 to use a calculator. Press q to quit.")
 
+// It's common practice to declare all variables at the beginning of the program
 var count = 1
 var calculator = 2
 var quit = q
